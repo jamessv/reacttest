ba2dba730e31b7db8d2c13b5713932ab0dadd105
jamessv <jamessrov>
2023-05-03 00:25:28 +0600

    2

---
+++ src/Framework/tst/tst2.js
@@ -0,0 +1,9 @@
+<button data-testid="ok-button" type="submit" disabled>ok</button>
+const button = getByTestId('ok-button')
+
+expect(button).toHaveAttribute('disabled')
+expect(button).toHaveAttribute('type', 'submit')
+expect(button).not.toHaveAttribute('type', 'button')
+
+expect(button).toHaveAttribute('type', expect.stringContaining('sub'))
+expect(button).toHaveAttribute('type', expect.not.stringContaining('but'))
