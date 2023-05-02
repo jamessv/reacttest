d81e503e7311ac7f626b54e44379b284e18ef003
jamessv <jamessrov>
2023-05-03 02:17:32 +0600

    new file added

--- main/cpy/file.ts
+++ main/cpy/file.ts
@@ -1,1 +1,6 @@
-this is ts file;
+describe('an essay on the best flavor', () => {
+    test('mentions grapefruit', () => {
+      expect(essayOnTheBestFlavor()).toMatch(/grapefruit/);
+      expect(essayOnTheBestFlavor()).toMatch(new RegExp('grapefruit'));
+    });
+  });
